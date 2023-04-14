de5263841ad009f1760f32576ec4bf004c32c6ef
Tamotsu Kawano <t.k.kyoto@gmail.com>
2023-04-14 17:45:37 +0900

    Initial Commit

---
+++ AllertOKButton/AllertOKButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllertOKButtonApp.swift
+//  AllertOKButton
+//
+//  Created by 河野保 on 2023/04/14.
+//
+
+import SwiftUI
+
+@main
+struct AllertOKButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
